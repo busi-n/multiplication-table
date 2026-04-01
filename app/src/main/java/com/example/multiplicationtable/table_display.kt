1d6a808fef1c9e97a54cb01187bf87e7af546032
busi-n <khulisanimsipa@gmail.com>
2026-04-01 16:05:26 +0200

    added switch between pages

--- app/src/main/java/com/example/multiplicationtable/table_display.kt
+++ app/src/main/java/com/example/multiplicationtable/table_display.kt
@@ -1,30 +1,17 @@
 package com.example.multiplicationtable
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class table_display : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val multiplyButton = findViewById<Button>(R.id.multiplyButton)
-        multiplyButton.setOnClickListener {
-
-            val intent = Intent(this, table_display::class.java)
-            startActivity(intent)
-        }
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clickMeBtn)) { v, insets ->
+        setContentView(R.layout.activity_table_display)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
