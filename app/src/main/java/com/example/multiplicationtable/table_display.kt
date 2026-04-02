bf9fb166929dc782a074600cbde469992e31a466
busi-n <khulisanimsipa@gmail.com>
2026-04-02 09:19:04 +0200

    bundle added

--- app/src/main/java/com/example/multiplicationtable/table_display.kt
+++ app/src/main/java/com/example/multiplicationtable/table_display.kt
@@ -11,6 +11,16 @@ class table_display : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_table_display)
+
+        //get the table number from the bundle
+        val bundle: Bundle? = intent.extras
+        val tableString: String? = bundle?.getString("tableNumber")
+
+        //convert the table number to an integer
+        val tableNumber = tableString?.toInt()
+
+
+        //dis
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
